bd32da203712ee622782907155a7c64fac50f08c
idriss96 <idrissatik1996@gmail.com>
2024-03-27 12:10:42 +0100

    add error handler to subscribeCard

--- Newsletter-sign-up-form-with-success-message/src/components/SubscribeCard.jsx
+++ Newsletter-sign-up-form-with-success-message/src/components/SubscribeCard.jsx
@@ -1,25 +1,64 @@
 import image from '../assets/images/illustration-sign-up-desktop.svg';
 import iconList from '../assets/images/icon-list.svg';
+import { useState } from 'react';
 
 function SubscribeCard() {
+  let email = '';
+  const [emailError, setEmailError] = useState(false);
+
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email = e.target[0].value;
+    console.log(email);
+    if (emailRegex.test(email)) {
+      setEmailError(false);
+    } else {
+      setEmailError(true);
+    }
+  };
+
   return (
     <div className="card-container">
       <div className="card-container__info">
-        <h1 className="title">
-          Stay updated!
-        </h1>
+        <h1 className="title">Stay updated!</h1>
         <p className="description">
           Join 60.000+ product managers receiving monthly updates on:
         </p>
         <ul className="list">
-          <li className='list-item'> <img src= {iconList} alt="check icon" /> Product discovery and building what matters</li>
-          <li className='list-item'> <img src= {iconList} alt="check icon" /> measuring to ensure updates are success</li>
-          <li className='list-item'> <img src= {iconList} alt="check icon" /> and much more!</li>
+          <li className="list-item">
+            <img src={iconList} alt="check icon" /> Product discovery and
+            building what matters
+          </li>
+          <li className="list-item">
+            <img src={iconList} alt="check icon" /> measuring to ensure updates
+            are success
+          </li>
+          <li className="list-item">
+            <img src={iconList} alt="check icon" /> and much more!
+          </li>
         </ul>
-        <form className="form">
-          <label htmlFor="email" className='form-label'>Email address</label>
-          <input className='form-input' type="email" placeholder="email@company.com" />
-          <button className='form-button' type="submit">Subscribe to monthly newsletter</button>
+        <form className="form" noValidate onSubmit={handleSubmit}>
+          <div className="form-label">
+            <label htmlFor="email" className="label-info">
+              Email address
+            </label>
+            {emailError ? (
+              <label htmlFor="email" className="label-error">
+                Valid email required
+              </label>
+            ) : null}
+          </div>
+          <input
+            className={emailError ? "form-input-error" : 'form-input'}
+
+            type="email"
+            placeholder="email@company.com"
+          />
+          <button className="form-button" type="submit">
+            Subscribe to monthly newsletter
+          </button>
         </form>
       </div>
       <div className="card-container__image">
